dc47e730b274806e5fbe6ea6830655ce421b1a75
anhhung04 <dev.hah4@gmail.com>
2024-06-25 16:26:51 +0700

    feat: setup route

--- backend-nosql/app.js
+++ backend-nosql/app.js
@@ -1,13 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+const Router = require('express').Router;
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/users');
+var authRouter = require('./routes/api/auth');
 
 var app = express();
+var api = Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+api.use('/auth', authRouter);
+api.use('/user', usersRouter);
+app.use('/api', api);
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
